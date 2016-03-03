f5fe3df3dd7a46a6421454fb0b5a4d87179e88ec
mr3m <carlos@prosciana.com>
2016-03-03 14:40:22 +0100

    ArticleID

--- articles_1.sql
+++ articles_1.sql
@@ -1,4 +1,6 @@
+SET @cnt = 0;
 CREATE TABLE articles_1 AS (
-SELECT  au.author, ar.title, ar.id
+SELECT  (@cnt := @cnt + 1) AS rowid, au.author, ar.title, ar.id
 FROM articles ar, articles_authors au
-WHERE ar.id = au.id )
+WHERE ar.id = au.id 
+)
